cb59e8ebd6d81703c57d9c159adb5073007f7239
William Camargo <williamcamargosilva@gmail.com>
2021-02-16 15:52:25 -0300

    created model in models of variables that will be used in api

---
+++ backend/WebAPI/Models/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Model
+    {
+        public int spId { get; set; }
+        public string spNameModel { get; set; }
+        public string spNameBland { get; set; }
+        public float spPrice { get; set; }
+    }
+}
