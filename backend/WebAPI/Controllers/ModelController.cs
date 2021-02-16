12ad280cc237c0b91c01496fb1164136abb027f0
William Camargo <williamcamargosilva@gmail.com>
2021-02-16 16:25:59 -0300

    inserted PUT method

--- backend/WebAPI/Controllers/ModelController.cs
+++ backend/WebAPI/Controllers/ModelController.cs
@@ -107,6 +107,36 @@ namespace WebAPI.Controllers
         }
 
 
+        [HttpPut]
+        public JsonResult Put(Model sp)
+        {
+            string query = @"
+                    update model set 
+                    spNameModel = '" + sp.spNameModel + @"'
+                    ,spNameBland = '" + sp.spNameBland + @"'
+                    ,spPrice = '" + sp.spPrice + @"'
+                    where spId = " + sp.spId + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ConexionBD");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
 
     }
 }
