b9c36722eb45a4ee229571570236f338b419ddff
William Camargo <williamcamargosilva@gmail.com>
2021-02-16 16:28:07 -0300

    inserted DELETE method

--- backend/WebAPI/Controllers/ModelController.cs
+++ backend/WebAPI/Controllers/ModelController.cs
@@ -136,7 +136,31 @@ namespace WebAPI.Controllers
             return new JsonResult("Updated Successfully");
         }
 
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from model
+                    where spId = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ConexionBD");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
 
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
 
     }
 }
