cf07b639c5ff1266a4e467abe4ef6ce899d7c37b
William Camargo <williamcamargosilva@gmail.com>
2021-02-16 16:04:31 -0300

    added ModelController file with IConfiguration and GET method and fixed close of keys in appsettings.json

---
+++ backend/WebAPI/Controllers/ModelController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ModelController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+
+            string query = @"
+                    select spId, spNameModel, spNameBland, spPrice from model";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ConexionBD");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+    }
+}
